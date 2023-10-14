7c3c41517c42c389a32bde5bb1d9b5a1cff50890
Dmitrii Khitryi <hdv.kub@gmail.com>
2023-10-14 16:49:48 +0300

    moved the function to utils.js

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,21 +1,13 @@
-import {Module} from '../core/module'
+import {Module} from '../core/module';
+import {getRandomColor} from '../utils';
 
 export class BackgroundModule extends Module {
     constructor(type, text) {
         super(type, text);
     }
-    /* Получение рандомного цвета, где
-    - 16777216 = 3 (RGB) * 256 (max значение для каждого цвета);
-    - (Math.random() * 16777216) - получаем случайное число в диапозоне от 0 до 16777215.99
-    - .toString(16) - преобразования числа в его шестнадцатеричное
-    */
-    getRandomColor() {
-        const randomColor = `#${Math.floor(Math.random() * 16777216).toString(16)}`;
-        return randomColor;
-    }
-
+ 
     trigger() {
-        document.body.style.backgroundColor = this.getRandomColor();
+        document.body.style.backgroundColor = getRandomColor();
     }
 }
         
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,13 @@
 export function random(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1))
 }
+
+/* Получение рандомного цвета, где
+    - 16777216 = 3 (RGB) * 256 (max значение для каждого цвета);
+    - (Math.random() * 16777216) - получаем случайное число в диапозоне от 0 до 16777215.99;
+    - .toString(16) - преобразования числа в его шестнадцатеричное
+    */
+export function getRandomColor() {
+      const randomColor = `#${Math.floor(Math.random() * 16777216).toString(16)}`;
+      return randomColor;
+}
