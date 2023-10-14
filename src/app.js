0bd7587de425cb187981e2a3aad7b1a1fee4df0f
Dmitrii Khitryi <hdv.kub@gmail.com>
2023-10-14 14:31:21 +0300

    Implemented background module

--- src/app.js
+++ src/app.js
@@ -1,2 +1,5 @@
 import './styles.css'
 
+import { BackgroundModule } from './modules/background.module';
+const backgroundModule = new BackgroundModule('random-background', 'Случайный фон');
+backgroundModule.trigger();
--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,22 @@
 import {Module} from '../core/module'
 
 export class BackgroundModule extends Module {
+    constructor(type, text) {
+        super(type, text);
+    }
+    /* Получение рандомного цвета, где
+    - 16777216 = 3 (RGB) * 256 (max значение для каждого цвета);
+    - (Math.random() * 16777216) - получаем случайное число в диапозоне от 0 до 16777215.99
+    - .toString(16) - преобразования числа в его шестнадцатеричное
+    */
+    getRandomColor() {
+        const randomColor = `#${Math.floor(Math.random() * 16777216).toString(16)}`;
+        return randomColor;
+    }
 
+    trigger() {
+        document.body.style.backgroundColor = this.getRandomColor();
+    }
 }
+        
+       
