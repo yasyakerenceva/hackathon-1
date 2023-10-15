0e9f62e52c6c851f46d3c8e1687ae123d14840a3
Islamov <diablomen3@gmail.com>
2023-10-15 17:23:40 +0800

    add import random function

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -1,11 +1,12 @@
 import { Module } from "../core/module";
+import { random } from "../utils";
 
 class SoundModule extends Module {
   constructor(type, text) {
     super(type, text);
   };
   trigger() {
-    const sound = new Audio(`./assets/sound/${Math.floor(Math.random() * (12 - 1) + 1)}.mp3`);
+    const sound = new Audio(`./assets/sound/${random(12, 1)}.mp3`);
     sound.addEventListener("canplaythrough", (event) => {
       sound.play();
     }, { once: true })
